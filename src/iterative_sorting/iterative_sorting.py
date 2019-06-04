79f76cc6ea96222690869634f38c35f4230ec023
Jor_Poon <jormpoon@gmail.com>
2019-06-04 03:23:39 -0400

    bubbe sort not completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,7 +6,7 @@ def selection_sort( arr ):
         smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        
+
         # looping through a second time for comparing int of smallest index to int of other indices
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[smallest_index]:
@@ -20,6 +20,16 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for i in range(len(arr)):
+        # cur_index = i
+        print("loop 1", i)
+        #comparing int of indexes in pairs then swapping the smaller number to the left side
+        # need to only compare with the next index and then move on 
+        # for j in range(i+1,):
+        #     if arr[j] < arr[i]:
+        #         arr[i], arr[j] = arr[j], arr[i]
+        #     print("loop 2", j)
+            
 
     return arr
 
